c07f39798ee2fe21072cfaa4930159456165559f
Nikola Zjalic <nzjalic@gmail.com>
2025-01-10 10:17:17 -0500

    navbar on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,21 @@ import FeaturesSection from "@/components/landing-page/features-section";
 import HeroSection from "@/components/landing-page/hero-section";
 import PricingSection from "@/components/landing-page/pricing-section";
 import SubscribeSection from "@/components/landing-page/subscribe-section";
+import Navbar from "@/components/nav-bar/nav-bar";
 
 export default async function LandingPage() {
   return (
-    <div className="flex flex-col min-h-screen bg-background text-foreground overflow-scroll w-full">
-      <main className="flex-1">
-        <HeroSection />
-        <FeaturesSection />
-        <PricingSection />
-        <SubscribeSection />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Navbar />
+      <div className="flex flex-col min-h-screen bg-background text-foreground overflow-scroll w-full">
+        <main className="flex-1">
+          <HeroSection />
+          <FeaturesSection />
+          <PricingSection />
+          <SubscribeSection />
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
