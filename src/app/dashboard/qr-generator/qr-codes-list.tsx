ad8aafd5cad6778aadf30b0d81524c065c41803b
Nikola Zjalic <nzjalic@gmail.com>
2025-01-22 02:09:22 -0500

    qr codes list

--- src/app/dashboard/qr-generator/qr-codes-list.tsx
+++ src/app/dashboard/qr-generator/qr-codes-list.tsx
@@ -1,5 +1,5 @@
 import { DashboardCard } from "@/components/dashboard-card/dashboard-card";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import Image from "next/image";
 
 const QR_CODES = [
@@ -14,6 +14,12 @@ const QR_CODES = [
 ];
 
 export const QRCodeList = () => {
+  const generateQRCode = (link: string) => {
+    const encodedLink = encodeURIComponent(link);
+    const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodedLink}&size=200x200`;
+    return qrCodeUrl;
+  };
+
   return (
     <DashboardCard
       title="QR Codes"
@@ -21,11 +27,21 @@ export const QRCodeList = () => {
     >
       {QR_CODES.map((qrCode, index) => (
         <Card key={qrCode.id} className="flex flex-col gap-2 ">
-          <div className="flex gap-2 p-4">
+          <div className="flex gap-16 p-4 items-center ">
             <h3>{index + 1}</h3>
-            <h3>{qrCode.url}</h3>
+            <div className="relative w-[100px] h-[100px] ">
+              <Image
+                src={generateQRCode(qrCode.url)}
+                alt="Generated QR Code"
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <h3>{qrCode.url}</h3>
+              <p>Clicks: 100</p>
+            </div>
           </div>
-          {/* <Image src={qrCode.url} alt={qrCode.url} width={200} height={200} /> */}
         </Card>
       ))}
     </DashboardCard>
