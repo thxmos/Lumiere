0104f7a6e69d47a724d4e74af52260b976f3431f
Nikola Zjalic <nzjalic@gmail.com>
2025-01-28 03:00:25 -0500

    add scans

--- src/app/[username]/qr/[qrId]/actions.ts
+++ src/app/[username]/qr/[qrId]/actions.ts
@@ -1,12 +1,19 @@
 "use server";
 
-import { getQRCodeById } from "@/data-access/qr-codes";
-import { updateQRCode } from "@/data-access/qr-codes";
+import { getQRCodeById, updateQRCode } from "@/data-access/qr-codes";
+import { createScan, CreateScanDto } from "@/data-access/scans";
+import type { BrowserData } from "@/types/clicks";
 
-export const updateQrScanAction = async (qrId: string) => {
+export const updateQrScanAction = async (
+  qrId: string,
+  browserData: Partial<BrowserData>,
+) => {
   const qrCode = await getQRCodeById(qrId);
   if (!qrCode) throw new Error("QR code not found");
   qrCode.scans = (qrCode.scans || 0) + 1;
   await updateQRCode(qrId, qrCode);
-  //   await createScan(qrId);
+  await createScan({
+    qrId,
+    ...browserData,
+  } as CreateScanDto);
 };
---
+++ src/data-access/scans.ts
@@ -0,0 +1,28 @@
+import { prisma } from "@/utils/prisma";
+
+export type CreateScanDto = {
+  qrId: string;
+  browser: string;
+  browserVersion: string;
+  operatingSystem: string;
+  deviceType: string;
+  screenResolution: string;
+  language: string;
+  timezone: string;
+};
+
+export const createScan = async (scan: CreateScanDto) => {
+  const newScan = await prisma.scan.create({
+    data: {
+      qrId: scan.qrId,
+      browser: scan.browser,
+      browserVersion: scan.browserVersion,
+      operatingSystem: scan.operatingSystem,
+      deviceType: scan.deviceType,
+      screenResolution: scan.screenResolution,
+      language: scan.language,
+      timezone: scan.timezone,
+    },
+  });
+  return newScan;
+};
