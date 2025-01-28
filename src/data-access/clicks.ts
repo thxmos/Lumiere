286b98ba0db394cc38f9fae3c1cfb27339b262a6
Nikola Zjalic <nzjalic@gmail.com>
2025-01-28 08:24:43 -0500

    fix build

--- src/data-access/clicks.ts
+++ src/data-access/clicks.ts
@@ -1,11 +1,13 @@
 import { prisma } from "@/utils/prisma";
 import { type BrowserData } from "@/types/clicks";
+import { Country } from "@prisma/client";
 
 export const createClick = async (linkId: string, data: BrowserData) => {
   await prisma.click.create({
     data: {
       linkId,
       ...data,
+      country: data.country as Country,
     },
   });
 };
