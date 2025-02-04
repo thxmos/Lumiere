957ddc0c975b15e1b74037d50b1773610ee11802
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2025-02-04 17:39:05 +0000

    feat: Create LinkGroupGrid component for displaying link groups

--- src/app/ulink/links/page.tsx
+++ src/app/ulink/links/page.tsx
@@ -1,24 +1,13 @@
 import { getLinkGroupsByUserId } from "@/actions/entities/link/getLinkGroupsByUserId";
-import Link from "next/link";
-import React from "react";
 import LinkGroupForm from "./link-group-form";
-import { Button } from "@/components/ui/button";
+import LinkGroupGrid from "../_components/link-group-grid";
 
 const LinkGroupsPage = async () => {
   const linkGroups = await getLinkGroupsByUserId();
 
   return (
     <div className="flex flex-col gap-4">
-      <p>Link Groups</p>
-      <div className="flex flex-wrap gap-2">
-        {linkGroups.map((group) => (
-          <Button variant="outline" key={group.id}>
-            <Link href={`/ulink/links/${group.id}`} key={group.id}>
-              {group.name}
-            </Link>
-          </Button>
-        ))}
-      </div>
+      <LinkGroupGrid linkGroups={linkGroups} />
       <LinkGroupForm />
     </div>
   );
