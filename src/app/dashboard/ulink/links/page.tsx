4a1e606bec94a57bb90f9f7140ad3493a66a9fa4
Nikola Zjalic <nzjalic@gmail.com>
2025-02-11 01:13:01 -0500

    few more

--- src/app/dashboard/ulink/links/page.tsx
+++ src/app/dashboard/ulink/links/page.tsx
@@ -3,13 +3,12 @@ import LinkGroupGrid from "./link-group-grid";
 import { getAssetsByUserId } from "@/actions/entities/asset/getAssetsByUserId";
 import { getThemeAction } from "@/actions/entities/theme/getTheme";
 import { getLinksByUserId } from "@/actions/entities/link/getLinksByUserId";
-import { getUserById } from "@/actions/entities/user/getUserById";
 
 const LinkGroupsPage = async () => {
-  const theme = await getThemeAction();
   const assets = await getAssetsByUserId();
   const links = await getLinksByUserId();
   const linkGroups = await getLinkGroupsByUserId();
+  const theme = await getThemeAction(); //TODO get by linkgroup
 
   return (
     <div className="flex flex-col gap-4">
